9dd8d44313a75c178961d728691753261be39d06
Richard Gaillet <gaillet.richard@gmail.com>
2019-03-19 17:29:02 +0100

    Add collaborators branch & rename photo

---
+++ collaborator.js
@@ -0,0 +1,42 @@
+const collaborators = [
+    {
+        firstName: "Richard",
+        lastName: "GAILLET",
+        photo: "image/RichardGaillet.jpg",
+        mail: "email@email.email",
+    },
+    {
+        firstName: "Alexandre",
+        lastName: "PASQUIER",
+        photo: "image/AlexandrePasquier.jpeg",
+        mail: "email@email.email",
+    },
+    {
+        firstName: "Loukiana",
+        lastName: "VIVENOT",
+        photo: "image/LoukianaVivenot.jpeg",
+        mail: "email@email.email",
+    },
+    {
+        firstName: "Maryline",
+        lastName: "ALVES",
+        photo: "image/MarylineAlves.jpeg",
+        mail: "email@email.email",
+    },
+    {
+        firstName: "Antoine",
+        lastName: "JACQMIN",
+        photo: "image/AntoineJacqmin.jpg",
+        mail: "email@email.email",
+    },
+    {
+        firstName: "Jordan",
+        lastName: "BONJEAN",
+        photo: "image/JordanBonjean.jpg",
+        mail: "email@email.email",
+    },
+];
+
+const photoColl = document.getElementById('photocoll');
+
+photoColl.inner
